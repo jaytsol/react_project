e09e1071c239af313be81a2f1e2ac13be6e849aa
jaytsol <jaytsol@naver.com>
2022-12-06 23:55:14 +0900

    style changed

--- src/Home.js
+++ src/Home.js
@@ -2,27 +2,28 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState();
-    const [isPending, setIsPending] = useState(true);
+  const [blogs, setBlogs] = useState();
+  const [isPending, setIsPending] = useState(true);
 
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-        .then(res => {
-            return res.json();
-        })
-        .then(data => {
-            console.log(data);
-            setBlogs(data);
-            setIsPending(false);
-        })
-    }, []);
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setBlogs(data);
+        setIsPending(false);
+      });
+    }, 1000);
+  }, []);
 
-    return (
-        <div className="home">
-            { isPending && <div>Loading...</div>}
-            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
-        </div>
-    )
+  return (
+    <div className="home">
+        { isPending && <div>Loading...</div>}
+        {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+    </div>
+  )
 }
  
 export default Home;
